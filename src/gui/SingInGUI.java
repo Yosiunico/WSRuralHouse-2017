4be547071436e6d6e0ff1baf63e94d4b9fcc3566
Cristian Rivas Gómez <rivasgomezcristian@gmail.com>
2017-02-21 17:19:08 +0100

    Añadida la clase SingInGUI en el paquete gui

---
+++ src/gui/SingInGUI.java
@@ -0,0 +1,46 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class SingInGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SingInGUI frame = new SingInGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SingInGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblEstaEsLa = new JLabel("This is SIngIn window");
+		lblEstaEsLa.setBounds(122, 69, 182, 81);
+		contentPane.add(lblEstaEsLa);
+	}
+}
